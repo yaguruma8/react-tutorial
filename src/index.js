a10d92c13c1afe99b282d4bb388aab92c7fb397b
yaguruma <yaguruma_y@yahoo.co.jp>
2022-04-11 00:49:13 +0900

    [add]react-tutorialの雛形をreact18に対応

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Square = () => {
+  return <button className='square'>{/* TODO */}</button>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Board = () => {
+  function renderSquare(i) {
+    return <Square />;
+  }
+
+  const status = 'Next player: X';
+
+  return (
+    <div>
+      <div className='status'>{status}</div>
+      <div className='board-row'>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className='board-row'>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className='board-row'>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+const Game = () => {
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board />
+      </div>
+      <div className='game-info'>
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
+
+
+// ======================================== render
+
+const container =  document.getElementById('root');
+const root = createRoot(container);
+root.render(<Game />)
+
